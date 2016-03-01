1f46d807b598a71fe1fb851fff50744f29d4accb
BoxRice924 <niuwa.ht@gmail.com>
2016-03-01 17:09:12 -0500

    Create 热门问题.js

---
+++ 热门问题.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".showContent").click(function(){        //当“展开全文”按钮点击的时候
+    $(".content").show();             //展示未完全显示的那部分内容
+    $(this).parent().hide();           //此处需要注意隐藏简略说明的文字，因为原来文字里面最后有省略号，不隐藏的后果就是展开后省略号仍旧在那里
+  });
+  $(".hideContent").click(function(){         //当“收起全文”按钮点击的时候
+    $(this).parent().hide();           //隐藏已经显示出来的文字
+    $(".showContent").parent().show();       //将简略说明的文字显示出来
+  });
+});
