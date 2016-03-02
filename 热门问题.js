e3059f2d54baaaca975c865fb3aba7cb775c55f0
BoxRice924 <niuwa.ht@gmail.com>
2016-03-02 16:53:16 -0500

    Update 热门问题.js

--- 热门问题.js
+++ 热门问题.js
@@ -1,4 +1,4 @@
-$(热门问题.html).ready(function(){
+$(p1).ready(function(){
   $(".showContent").click(function(){        //当“展开全文”按钮点击的时候
     $(".content").show();             //展示未完全显示的那部分内容
     $(this).parent().hide();           //此处需要注意隐藏简略说明的文字，因为原来文字里面最后有省略号，不隐藏的后果就是展开后省略号仍旧在那里
