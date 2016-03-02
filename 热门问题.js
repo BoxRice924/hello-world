2b59fc7b44eb790264c4617a0a930af343f07d19
BoxRice924 <niuwa.ht@gmail.com>
2016-03-02 17:15:50 -0500

    Update 热门问题.js

--- 热门问题.js
+++ 热门问题.js
@@ -1,4 +1,4 @@
-$(热门问题.html).ready(function(){
+$(document).ready(function(){
   $(".showContent").click(function(){        //当“钱箱打不开”按钮点击的时候
     $(".content").show();             //展示未完全显示的那部分内容
     $(this).parent().hide();           //此处需要注意隐藏简略说明的文字，因为原来文字里面最后有省略号，不隐藏的后果就是展开后省略号仍旧在那里
