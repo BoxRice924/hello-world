4a3e45ada4e8d6a80bb89f9cf5997c80447bef3b
BoxRice924 <niuwa.ht@gmail.com>
2016-03-08 10:46:00 -0500

    Create Menusifu365-questions.js

---
+++ Menusifu365-questions.js
@@ -0,0 +1,10 @@
+
+function closeMe(){
+  x=document.getElementById("CloseContent");
+  x.className="closed";
+}
+
+function openMe(){
+  x=document.getElementById("questions");
+  x.className="Open";
+}
