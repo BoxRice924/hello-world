691990f914c89cc8777bbc5abc85a8ad1089b6f4
BoxRice924 <niuwa.ht@gmail.com>
2016-03-08 17:10:13 -0500

    Update Menusifu365-questions.js

--- Menusifu365-questions.js
+++ Menusifu365-questions.js
@@ -1,10 +1,14 @@
 
-
-function opencloseMe(){
-  var ContDisplay = document.getElementById("content").style.display
-  if (ContDisplay == "none")
-  {ContDisplay = "block";
-}
-else
-{ ContDisplay = "none"};
-}
+ function opencloseMe(){
+ if (document.getElementById("content").style.display = "none")
+ {x=document.getElementById("content");
+ x.className="open";
+ var ContDisplay = document.getElementById("content").style.display
+ if (ContDisplay == "none")
+ {ContDisplay = "block";
+  }
+  else
+ { x=document.getElementById("content");
+   x.className="closed"};
+ { ContDisplay = "none"};
+  }
