f1c912b1aacb08e84dd2c107d853a69943a8ed66
BoxRice924 <niuwa.ht@gmail.com>
2016-03-08 17:05:35 -0500

    Update Menusifu365-questions.js

--- Menusifu365-questions.js
+++ Menusifu365-questions.js
@@ -1,11 +1,10 @@
 
 
 function opencloseMe(){
-  if (document.getElementById("content").style.display = "none")
-  {x=document.getElementById("content");
-  x.className="open";
+  var ContDisplay = document.getElementById("content").style.display
+  if (ContDisplay == "none")
+  {ContDisplay = "block";
 }
 else
-{ x=document.getElementById("content");
-  x.className="closed"};
+{ ContDisplay = "none"};
 }
